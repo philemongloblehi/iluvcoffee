97065702ba04d60e9dbdc68b9d145eff05933784
Philemon GLOBLEHI <philemongloblehi@gmail.com>
2020-08-10 17:28:13 +0000

    Initial commit

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,33 @@
+import {Body, Controller, Delete, Get, Param, Patch, Post, Query} from '@nestjs/common';
+
+@Controller('coffees')
+export class CoffeesController {
+    @Get()
+    findAll(@Query() paginationQuery): string
+    {
+        const {limit, offset} = paginationQuery;
+        return `This action returns all coffees. Limit: ${limit}, offset: ${offset}`;
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string): string {
+        return `This action #${id} coffee`;
+    }
+
+    @Post()
+    create(@Body() body: string): any
+    {
+        return body;
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() body: string): string
+    {
+        return `This action updates #${id} coffee`;
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return `This action removes #${id} coffee`;
+    }
+}
